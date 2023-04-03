6b0f9c25e1494175a0a4c49be5a7255ed9c11fea
Micatiau <mikaelitakara@gmail.com>
2023-04-03 08:39:00 -0300

    Nosso jogo finalizado

--- nossoJogo.js
+++ nossoJogo.js
@@ -1,3 +1,37 @@
+console.log("Boas vindas ao jogo de BlackJack!")
+let inicio = confirm("Quer iniciar uma nova rodada?")
+
+if(inicio === false) {
+
+   console.log("O jogo acabou!")
+
+}else{
+
+   const priCartaJogador = comprarCarta()
+   const segCartaJogador = comprarCarta()
+   const pontuacaojogador = priCartaJogador.valor + segCartaJogador.valor
+
+   console.log(`Jogador: ${priCartaJogador.texto} ${segCartaJogador.texto} - pontuação final: ${pontuacaojogador}`)
+
+   const priCartaCPU = comprarCarta()
+   const segCartaCPU = comprarCarta()
+   const pontuacaoCPU = priCartaCPU.valor + segCartaCPU.valor
+
+   console.log(`Computador: ${priCartaCPU.texto} ${segCartaCPU.texto} - pontuação final: ${pontuacaoCPU}`)
+   
+   if(pontuacaojogador > pontuacaoCPU && pontuacaojogador <= 21) {
+      console.log("Você ganhou!")
+   } else if(pontuacaoCPU > pontuacaojogador && pontuacaoCPU <= 21){
+      console.log("O computador ganhou!")
+   } else if(pontuacaojogador > pontuacaoCPU && pontuacaojogador > 21){
+      console.log("Você passou!")
+   } else if(pontuacaoCPU > pontuacaojogador && pontuacaoCPU > 21){
+      console.log("O computador passou!")
+   } else if (pontuacaoCPU <= 21 && pontuacaojogador <= 21){
+      console.log("Empate!")
+   }
+}
+
 /**
  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
  * 
