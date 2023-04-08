1ab754a1045699c85abbb6e65c7ae983e34803fa
Micatiau <mikaelitakara@gmail.com>
2023-04-07 21:57:54 -0300

    desafio finalizado

--- desafio.js
+++ desafio.js
@@ -1,13 +1,111 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+let jogador = []; //variaveis com arrays vazias
+let cpu = [];
+let inicio = confirm("Bem vindo ao BlackJack! \nQuer iniciar uma nova rodada?"
+);
 
+if (inicio) {
+
+  // VERIFICAÇÃO DOS ÁSES //
+
+  let cartasIguais = true;
+  while (cartasIguais) {
+    jogador = [];
+    cpu = [];
+
+    for (let i = 0; i < 2; i++) {
+      jogador.push(comprarCarta());
+      cpu.push(comprarCarta());
+    }
+
+    if (
+      (jogador[0].valor === 11 && jogador[1].valor === 11) ||
+      (cpu[0].valor === 11 && cpu[1].valor === 11)
+    ) {
+
+      jogador = [];
+      cpu = [];
+    } else {
+
+      cartasIguais = false;
+
+    }
+
+  }
+
+  // DAR CARTA //
+
+
+  let textoCartaJogador = "";
+  let textoCartasCPU = "";
+  let pontuacaoJogador = 0;
+  let pontuacaoCPU = 0;
+
+  for (let i = 0; i < jogador.length; i++) {
+    pontuacaoJogador += jogador[i].valor;
+    textoCartaJogador += jogador[i].texto + " ";
+
+  }
+
+  for (let j = 0; j < cpu.length; j++) {
+    if (j < jogador.length) {
+      pontuacaoCPU += cpu[j].valor;
+      textoCartasCPU += cpu[j].texto + " ";
+
+    }
+    
+  }
+
+  let outraCarta = true;
+    // COMPRAR CARTA JOGADOR
+  while (outraCarta && pontuacaoJogador < 22) {const confirmarOutraCarta = confirm(`Suas cartas são ${textoCartaJogador}. A carta revelada do computador é ${cpu[0].texto}. \nDeseja comprar mais uma carta?`);
+
+    if (confirmarOutraCarta) {
+
+      let outraCarta = comprarCarta();
+      jogador.push(outraCarta);
+      pontuacaoJogador += outraCarta.valor;
+      textoCartaJogador += outraCarta.texto;
+
+    } else {
+
+      outraCarta = false;
+    }
+
+  }
+
+  //CPU JOGA A MSM QUANTIDADE DE CARTAS QUE O JOGADOR
+
+  while (pontuacaoCPU <= pontuacaoJogador && pontuacaoJogador < 21) {
+    let outrasCartas1 = comprarCarta();
+    cpu.push(outrasCartas1);
+    pontuacaoCPU += outrasCartas1.valor;
+    textoCartasCPU += outrasCartas1.texto + " ";
+  }
+
+
+  let finalPartida = " ";
+    // PARAMENTROS DE PONTUAÇÃO
+  if (pontuacaoJogador > pontuacaoCPU && pontuacaoJogador <= 21) {
+    finalPartida = "Você ganhou!";
+    outraCarta = false;
+  } else if (pontuacaoCPU > pontuacaoJogador && pontuacaoCPU <= 21) {
+    finalPartida = "O computador ganhou!";
+    outraCarta = false;
+  } else if (pontuacaoJogador > 21) {
+    finalPartida = "Você perdeu!";
+    outraCarta = false;
+  } else if (pontuacaoCPU > 21) {
+    finalPartida = "Você perdeu!";
+    outraCarta = false;
+  } else {
+    finalPartida = "Empate!";
+  }
+
+  alert(`Usuario - Cartas: ${textoCartaJogador} - Pontuação: ${pontuacaoJogador} \nComputador - Cartas: ${textoCartasCPU} - Pontuação: ${pontuacaoCPU} \n${finalPartida}`
+  )
+
+} else {
+
+  alert(`Fim de Jogo`);
+
+}
--- nossoJogo.js
+++ nossoJogo.js
@@ -1,7 +1,7 @@
 console.log("Boas vindas ao jogo de BlackJack!")
 let inicio = confirm("Quer iniciar uma nova rodada?")
 
-if(inicio === false) {
+if(!inicio) {
 
    console.log("O jogo acabou!")
 
@@ -31,16 +31,3 @@ if(inicio === false) {
       console.log("Empate!")
    }
 }
-
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
